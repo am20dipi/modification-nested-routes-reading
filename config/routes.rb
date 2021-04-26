5accf98ed1c251b18bffa6c50e8d3a478fe1897d
Adriana <am20dipi@alum.siena.edu>
2021-04-26 14:46:19 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+  end
+  # '/authors/:author_id/posts/new'  new_author_post_path  NEW
+  # '/authors/:author_id/post'  author_post_path(post)  SHOW 
+  # '/authors/:author_id/posts author_posts_path   INDEX
+  # '/authors/:author_id/posts/edit' edit_author_post_path(post)  EDIT
+  
+
+  resources :posts 
 end
